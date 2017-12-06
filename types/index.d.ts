29fdb03b12b2aa43880ade1eadfc063420712955
Sean Drumm <sean@seandrumm.co.uk>
2017-12-06 10:19:49 +0000

    Add TypeScript types definition

---
+++ types/index.d.ts
@@ -0,0 +1,24 @@
+/// <reference types="node" />
+/// <reference types="koa" />
+
+declare module "koa-json-logger-next" {
+  
+      import * as Koa from "koa";
+      
+      interface LoggerOptions {
+          name?: string;
+          path?: string | null;
+          json?: boolean;
+          surfaceErrors?: boolean;
+          isJson?: () => boolean;
+      }
+  
+      /**
+       * JSON Logger middleware for Koa
+       */
+      function logger(options?: LoggerOptions): Koa.Middleware;
+  
+      namespace logger {}
+      export = logger;
+  }
+  
